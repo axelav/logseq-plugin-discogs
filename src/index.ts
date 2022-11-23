0df073a836e34ea821e9226db8bed6def82f0375
Axel Anderson <axel@ftml.net>
2022-11-23 08:18:04 -0500

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,81 @@
+import '@logseq/libs'
+import { BlockIdentity } from '@logseq/libs/dist/LSPlugin.user'
+
+const handleResponse = async (res: Response) => {
+  if (!res.ok) {
+    const text = await res.text()
+
+    return Promise.reject(text)
+  } else {
+    return await res.json()
+  }
+}
+
+const fetchData = async (e: { uuid: string }) => {
+  const query = (await logseq.Editor.getBlock(e.uuid))?.content.split('\n')[0]
+
+  if (query) {
+    try {
+      const res = await fetch(`https://www.honkytonk.in/api/discogs?q=${query}`)
+      const release = await handleResponse(res)
+
+      if (!release) {
+        return logseq.UI.showMsg('logseq-discogs-plugin :: No results!')
+      }
+
+      addRelease(release, e.uuid)
+    } catch (err) {
+      console.error('logseq-discogs-plugin :: Error: ', err)
+    }
+  }
+}
+
+interface Release {
+  artist: string
+  title: string
+  year: number
+  tags: string[]
+  label: string
+  url: string
+}
+
+const addRelease = async (release: Release, srcBlock: BlockIdentity) => {
+  const { artist, title, year, tags, label } = release
+  const mainText = `${artist}, *${title}* ([[${year}]])`
+
+  await logseq.Editor.insertBatchBlock(srcBlock, [
+    {
+      content: mainText,
+      children: [
+        {
+          content: 'rating:: ',
+        },
+        {
+          content: `record-label:: [[${label}]]`,
+        },
+        {
+          content: `tags:: ${tags.join(', ')}`,
+        },
+        {
+          content: `url:: ${release.url}`,
+        },
+      ],
+    },
+  ])
+
+  await logseq.Editor.removeBlock(srcBlock)
+}
+
+const main = () => {
+  console.log('logseq-discogs-plugin :: Loaded!')
+
+  logseq.Editor.registerBlockContextMenuItem(
+    'Query discogs.com API',
+    async (e) => {
+      console.log('logseq-discogs-plugin :: Fetching results...')
+      await fetchData(e)
+    }
+  )
+}
+
+logseq.ready(main).catch(console.error)
